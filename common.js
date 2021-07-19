da5bf9a508627e9c7e3de4651775878f4d24de67
hemant-batra <hemant.batra@theinnovationinc.co>
2021-07-19 21:59:15 +0530

    commit

--- common.js
+++ common.js
@@ -1,15 +1,24 @@
-function getElement(id) {
-    return window.parent.document.getElementById(id);
-}
 function getApiUrl() {
-    return getElement('apiUrl');
+    //return window.parent.document.getElementById('apiUrl');
+    return 'http://openqa.tab.kitecash.in';
 }
+
 function getBasicAuthToken() {
-    return getElement('basicAuthToken');
+    //return window.parent.document.getElementById('basicAuthToken');
+    return 'dW5vY29pbjpEbnowIWFkbW4lOCMy';
 }
+
 function getUserId() {
-    return getElement('userId');
+    return window.parent.document.getElementById('userId');
 }
+
 function getCardId() {
-    return getElement('cardId');
+    return window.parent.document.getElementById('cardId');
+}
+
+function callApi(httpMethod, apiEndpoint, requestJson) {
+    window.top.postMessage('httpMethod||' + httpMethod, '*');
+    window.top.postMessage('apiEndpoint||' + apiEndpoint, '*');
+    window.top.postMessage('requestJson||' + requestJson, '*');
+    setTimeout(() => window.top.postMessage('send||send', '*'), 200);
 }
