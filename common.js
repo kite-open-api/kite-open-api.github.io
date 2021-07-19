1af7749b0fdf59a2bc5bd12b72d29bf939a2d601
hemant-batra <hemant.batra@theinnovationinc.co>
2021-07-20 00:14:42 +0530

    generate otp

--- common.js
+++ common.js
@@ -10,10 +10,12 @@ function getCardId() {
     return window.parent.document.getElementById('cardId');
 }
 
-function callApi(httpMethod, apiEndpoint, requestJson) {
+function callApi(httpMethod, apiEndpoint, requestJson, userIdRequired, cardIdRequired) {
     window.parent.document.getElementById('responseJson').value = 'Processing...';
     window.top.postMessage('httpMethod||' + httpMethod, '*');
     window.top.postMessage('apiEndpoint||' + apiEndpoint, '*');
     window.top.postMessage('requestJson||' + requestJson, '*');
+    window.top.postMessage('userIdRequired||' + userIdRequired, '*');
+    window.top.postMessage('cardIdRequired||' + cardIdRequired, '*');
     setTimeout(() => window.top.postMessage('send||send', '*'), 200);
 }
